c58300d0e3a0a253e982ee4571c57f4787576e4e
naimdevweb <naimhamidi19@gmal.com>
2024-12-04 14:27:08 +0100

    bonus dico fini

---
+++ index.php
@@ -0,0 +1,39 @@
+ <?php
+$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
+$dico = explode("\n", $string); -->
+
+// echo count($dico);
+
+// $compte = 0;
+// foreach ($dico as $mot) {
+//     $mot = trim($mot);
+//     if (strlen($mot) == 15) {
+//         $compte++;
+//     }
+// }
+// echo "Il y a $compte mots de 15 caractères exactement.";
+
+
+
+// $compte = 0;
+// foreach ($dico as $mot) {
+    
+//     if (str_contains($mot,"w")) {
+//         $compte++;
+//     }
+// }
+// echo "Il y a $compte mots qui contiennent la lettre w.";
+
+// $compte = 0;
+// foreach ($dico as $mot) {
+//     $mot = trim($mot);
+//     if (str_ends_with($mot,"q")) {
+//         $compte++;
+//     }
+// }
+// echo "Il y a $compte mots qui termine par la lettre q.";
+
+
+
+ ?>
+
